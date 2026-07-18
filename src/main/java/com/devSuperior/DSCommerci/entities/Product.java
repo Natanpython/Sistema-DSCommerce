bbc57ed58d86a2d9abadac5abc4c0ffe971dac56
NatanaelDev <natanaelpereira745@gmail.com>
2026-07-18 16:40:40 -0300

    fix/OrderItem, OrderItemPK

--- src/main/java/com/devSuperior/DSCommerci/entities/Product.java
+++ src/main/java/com/devSuperior/DSCommerci/entities/Product.java
@@ -18,7 +18,7 @@ public class Product {
 
     @Column(columnDefinition = "TEXT")
     private String description;
-    
+
     private Double price;
     private String imgUrl;
 
@@ -28,6 +28,8 @@ public class Product {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -85,4 +87,17 @@ public class Product {
     public Set<Category> getCategoriess() {
         return this.categories;
     }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+    
+    public List<Order> getOrders(){
+        return  items.stream().map(x -> x.getOrder()).toList();
+    }
+
 }
