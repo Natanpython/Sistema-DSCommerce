dbb7ce19cb075444000514f7a6e6a959243344a7
NatanaelDev <natanaelpereira745@gmail.com>
2026-07-22 17:59:58 -0300

    fix/Busca paginada de produtos

--- src/main/java/com/devSuperior/DSCommerci/controller/ProductController.java
+++ src/main/java/com/devSuperior/DSCommerci/controller/ProductController.java
@@ -3,12 +3,14 @@ package com.devSuperior.DSCommerci.controller;
 import com.devSuperior.DSCommerci.dto.ProductDTO;
 import com.devSuperior.DSCommerci.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -17,8 +19,17 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+//Buscar pelo ID
     @GetMapping(value ="/{id}")
     public ProductDTO findById(@PathVariable Long id){
        return service.findById(id);
     }
+
+//Buscar a lista completa
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
+    }
+
+
 }
--- src/main/java/com/devSuperior/DSCommerci/repositories/ProductRepository.java
+++ src/main/java/com/devSuperior/DSCommerci/repositories/ProductRepository.java
@@ -3,7 +3,7 @@ package com.devSuperior.DSCommerci.repositories;
 import com.devSuperior.DSCommerci.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductReposutory extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
 }
