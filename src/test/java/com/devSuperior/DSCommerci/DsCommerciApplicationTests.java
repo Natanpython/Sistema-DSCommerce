5fa98728959e2e3affae488ca08bfba9db4f8e31
NatanaelDev <natanaelpereira745@gmail.com>
2026-09-06 12:20:55 -0300

    feature/Implementação de sistema de segurança Oauth2 e JWT

--- src/test/java/com/devSuperior/DSCommerci/DsCommerciApplicationTests.java
+++ src/test/java/com/devSuperior/DSCommerci/DsCommerciApplicationTests.java
@@ -3,7 +3,7 @@ package com.devSuperior.DSCommerci;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DsCommerciApplicationTests {
 
 	@Test
